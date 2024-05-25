2c91702ca0cec321ba2e247009eafdb92fe7c7ce
Sergio de la Torre <sergio.delatorre@unmsm.edu.pe>
2024-05-25 11:31:21 -0500

    Project Setup

---
+++ src/main/java/com/openpay/marvelapi/client/MarvelApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.openpay.marvelapi.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openpay/marvelapi/client/MarvelApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openpay.marvelapi.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
